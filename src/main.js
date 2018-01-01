863073b99739b2e5d3be7eef21425114b52aaa7c
baihe <baihe@macdeiMac.lan>
2018-01-02 00:29:02 +0800

    updated

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 
@@ -16,7 +15,6 @@ import 'element-ui/lib/theme-default/index.css'
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
-Vue.use(Vuex)
 // Vue.use(VueAxios, axios)
 // Vue.axios.defaults.headers.post['Content-Type'] = 'application/json'
 
