b1b05d89945c8c6d2a06e24a189943358edc5f54
baihe <baihe@macdeiMac.lan>
2018-01-02 23:52:39 +0800

    updated

--- src/views/ExecDetail.vue
+++ src/views/ExecDetail.vue
@@ -13,6 +13,7 @@ export default {
     return {
       tasks: [],
       deps: {},
+      execSnap: {},
       taskItemProps: {
         children: "deps",
         label: "name"
@@ -20,23 +21,11 @@ export default {
       flowLoading: true
     };
   },
-  // computed: {
-  //   taskList() {
-  //     return this.tasks.map((task, idx) => {
-  //       return {
-  //         name: "#" + idx + ". " + task,
-  //         deps:
-  //           task in this.deps
-  //             ? this.deps[task].map(dep => {
-  //                 return {
-  //                   name: dep
-  //                 };
-  //               })
-  //             : []
-  //       };
-  //     });
-  //   }
-  // },
+  computed: {
+    execState() {
+      return this.execSnap
+    }
+  },
   methods: {
     refreshGraph() {
       // 第三步：设置数据
@@ -44,25 +33,31 @@ export default {
         return {
           id: task,
           label: task,
-          shape: "rect"
+          shape: "rect",
+          color: this.execState[task] == 1 ? 'grey' :
+                 this.execState[task] == 2 ? 'blue' :
+                 this.execState[task] == 3 ? 'green' :
+                 this.execState[task] == 4 ? 'red' : 'yellow'
         };
       });
 
       nodes.push({
-          id: this.flow,
-          label: this.flow,
-          shape: "rect"
-      })
+        id: this.flow,
+        label: this.flow,
+        shape: "rect"
+      });
 
-      var edges = Array.concat(...Object.keys(this.deps).map(target => {
+      var edges = Array.concat(
+        ...Object.keys(this.deps).map(target => {
           return this.deps[target].map(source => {
-              return {
-                source: source,
-                target: target,
-                id: source + '-' + target
-              }
-          })
-      }))
+            return {
+              source: source,
+              target: target,
+              id: source + "-" + target
+            };
+          });
+        })
+      );
 
       // 第四步：配置G6画布
       const miniMap = new Plugins["tool.minimap"]();
@@ -70,30 +65,36 @@ export default {
         rankdir: "TB",
         nodesep: 100,
         ranksep: 200
-      })
+      });
       const net = new G6.Net({
         id: "tg", // 容器ID
         width: window.innerWidth, // 画布宽
         height: 600, // 画布高
-        fitView: 'cc',
+        fitView: "cc",
         // grid: null,
         // useAnchor: false,
         plugins: [miniMap, dagre]
-      })
-      net.changeMode('drag')
+      });
+      net.changeMode("drag");
 
-      net.source(nodes, edges)
-      net.render()
+      net.source(nodes, edges);
+      net.render();
     }
   },
   mounted() {
     API.LOAD_EXEC.issue({ id: this.execId }).then(response => {
-      var flow = response.data.flow
-      this.tasks = flow.tasks
-      this.deps = flow.deps
-      this.flow = flow.name
-      this.refreshGraph()
-      this.flowLoading = false
+      const flow = response.data.flow;
+      this.tasks = flow.tasks;
+      this.deps = flow.deps;
+      this.flow = flow.name;
+
+      const tasks = response.data.exec.tasks;
+      for (let t of tasks) {
+        this.execSnap[t['task']] = t['status']
+      }
+      console.log(this.execSnap)
+      this.refreshGraph();
+      this.flowLoading = false;
     });
   }
 };
